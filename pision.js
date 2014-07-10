405a7799d6bdbf3072d1fb89dff0e4fb10e86194
Matt Lacey <lacey@spkeasey.com>
2014-07-11 00:03:33 +1000

    Simple list of directories and .js files

--- pision.js
+++ pision.js
@@ -1,40 +1,60 @@
+var express = require("express");
+var handlebars = require("express3-handlebars");
 var fs = require("fs");
 var path = require("path");
 
-var dir = (process.argv.length > 2 ? process.argv[2] : ".");
+var app = express();
 
-console.log("Listing of: " + dir);
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-fs.readdir(dir, function(err, files)
+var startDir = (process.argv.length > 2 ? process.argv[2] : ".");
+var dirs, disks;
+
+app.get('/', function(req, res)
 {
-	if(err)
-	{
-		console.log(err);
-	}
-	else
-	{
-		var disks = files.filter(function(value, index, array)
-		{
-			return path.extname(value) == '.js';
-		});
-
-		var dirs = files.filter(function(value, index, array)
-		{
-			return path.extname(value) == '';
-		});
-
-		console.log('Directories:\n');
-
-		for(var i = 0; i < dirs.length; i++)
-		{
-			console.log(dirs[i]);
-		}
-
-		console.log('\n\nDisks:\n');
-
-		for(var i = 0; i < disks.length; i++)
-		{
-			console.log(disks[i]);
-		}
-	}
+	res.render('home', {dirs: dirs, disks: disks});
 });
+
+function listDirectory(dir)
+{
+	console.log("Listing of: " + dir);
+
+	fs.readdir(dir, function(err, files)
+	{
+		if(err)
+		{
+			console.log(err);
+		}
+		else
+		{
+			disks = files.filter(function(value, index, array)
+			{
+				return path.extname(value) == '.js';
+			});
+
+			dirs = files.filter(function(value, index, array)
+			{
+				return path.extname(value) == '';
+			});
+
+			console.log('Directories:\n');
+
+			for(var i = 0; i < dirs.length; i++)
+			{
+				console.log(dirs[i]);
+			}
+
+			console.log('\n\nDisks:\n');
+
+			for(var i = 0; i < disks.length; i++)
+			{
+				console.log(disks[i]);
+			}
+		}
+	});
+}
+
+listDirectory(startDir);
+
+app.listen(3000);
