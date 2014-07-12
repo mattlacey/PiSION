f210732e39817b2c9473122a31f32191af01bb10
Matt Lacey <lacey@spkeasey.com>
2014-07-12 15:39:02 +1000

    First hacky pass at directory navigation and file launching.

--- pision.js
+++ pision.js
@@ -1,60 +1,111 @@
-var express = require("express");
-var handlebars = require("express3-handlebars");
-var fs = require("fs");
-var path = require("path");
+
+/*jshint strict:false*/
+/*jshint node:true*/
+var express = require('express');
+var handlebars = require('express3-handlebars');
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec, child;
 
 var app = express();
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var startDir = (process.argv.length > 2 ? process.argv[2] : ".");
-var dirs, disks;
+var startDir = (process.argv.length > 2 ? process.argv[2] : '.');
+var dirStack = [];
+
+var data =
+{
+	dirs: [],
+	disks: [],
+	message: ''
+};
 
 app.get('/', function(req, res)
 {
-	res.render('home', {dirs: dirs, disks: disks});
+	listDirectory(startDir, res);
 });
 
-function listDirectory(dir)
+app.get('/load', function(req, res)
 {
-	console.log("Listing of: " + dir);
+	var disk = data.disks[req.query.disk];
+
+	child = exec('sio2bsd ' + disk, function (error, stdout, stderr)
+	{
+    	if(error !== null)
+    	{
+			console.log('exec error: ' + error);
+			data.message = 'Error: ' + error;
+		}
+		else
+		{
+			data.message = 'Loaded: ' + disk;		
+		}
+
+		res.render('home', data);
+    });
+});
+
+app.get('/dir', function(req, res)
+{
+	var dir = data.dirs[req.query.dir];
+	listDirectory(dir, res);
+});
+
+app.get('/up', function(req, res)
+{
+
+	if(dirStack.length > 1)
+	{
+		dirStack.pop();
+
+		listDirectory(dirStack[dirStack.length - 1], res);
+	}
+	else
+	{
+		listDirectory(startDir, res);
+	}
+});
+
+function listDirectory(dir, res)
+{
+	console.log('Listing of: ' + dir);
+	
+	if(dir != dirStack[dirStack.length - 1])
+	{
+		dirStack.push(dir);
+	}
+
+	dir = dirStack.join('/');
 
 	fs.readdir(dir, function(err, files)
 	{
 		if(err)
 		{
 			console.log(err);
+			data.message = 'Error: ' + err;
 		}
 		else
 		{
-			disks = files.filter(function(value, index, array)
+			data.disks = files.filter(function(value)
 			{
-				return path.extname(value) == '.js';
+				return path.extname(value) !== '';
 			});
 
-			dirs = files.filter(function(value, index, array)
+			data.dirs = files.filter(function(value)
 			{
-				return path.extname(value) == '';
+				return path.extname(value) === '';
 			});
 
-			console.log('Directories:\n');
+			data.message = 'Current Directory: ' + dir;			
+		}
 
-			for(var i = 0; i < dirs.length; i++)
-			{
-				console.log(dirs[i]);
-			}
-
-			console.log('\n\nDisks:\n');
-
-			for(var i = 0; i < disks.length; i++)
-			{
-				console.log(disks[i]);
-			}
+		if(res)
+		{
+			res.render('home', data);
 		}
 	});
 }
-
-listDirectory(startDir);
 
 app.listen(3000);
